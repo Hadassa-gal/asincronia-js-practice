04a700e5d0ada172947b3edb7ed800c4bebac5db
Hadassa-gal <hadassa.rgr@gmail.com>
2025-04-30 16:01:27 -0500

    changes

--- src/main.js
+++ src/main.js
@@ -12,55 +12,50 @@ document.querySelector('#calcular').addEventListener('click',(e) =>
   if (imc < 19.8) {
     imcr = 'bajo peso';
     divInferior.innerHTML = `
-    <div>
+    <div class="nose">
       <div class="text">
         <p class="peso">${imcr}</p>
         <p class="imc">${imc}</p>
       </div>
-      
+      <img src="./src/storage/baix-pes2.jpg" class="imgs"></img>
     </div>
-    <img src="./src/storage/baix-pes2.jpg" class="imgs"></img>
+    
     `
   } else if (imc >= 19.9 && imc <= 26) {
     imcr = 'peso normal';
     divInferior.innerHTML = `
-    <div>
+    <div class="nose">
       <div class="text">
         <p class="peso">${imcr}</p>
         <p class="imc">${imc}</p>
       </div>
-      
+      <img src="./src/storage/pesonormal.jpg" class="imgs"></img>
     </div>
-    <img src="./src/storage/pesonormal.jpg" class="imgs"></img>
+    
     `
   } else if (imc >= 26.1 && imc <= 29) {
     imcr = 'sobrepeso';
     divInferior.innerHTML = `
-    <div>
+    <div class="nose">
       <div class="text">
         <p class="peso">${imcr}</p>
         <p class="imc">${imc}</p>
       </div>
-      
+      <img src="./src/storage/images.jpg" class="imgs"></img>
     </div>
-    <img src="./src/storage/images.jpg" class="imgs"></img>
     `
   } else if (imc >= 29.1) {
     imcr = 'obesidad';
     divInferior.innerHTML = `
-    <div>
+    <div class="nose">
       <div class="text">
         <p class="peso">${imcr}</p>
         <p class="imc">${imc}</p>
       </div>
-      
+      <img src="./src/storage/images (1).jpg" class="imgs"></img>
     </div>
-    <img src="./src/storage/images (1).jpg" class="imgs"></img>
     `
   }
-  
-    
-   alert(imc) 
    console.log(imcr);
   e.preventDefault();
 })
