6bd68d95fc5fb4996ca041375dd26b1e1984f556
Hadassa-gal <hadassa.rgr@gmail.com>
2025-05-01 13:58:55 -0500

    optinización de la parte para mostrar el resultado

--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
 const boton= document.getElementById('calcular');
 const form = document.querySelector("#form");
 const divInferior = document.querySelector("#hidden");
-
-document.querySelector('#calcular').addEventListener('click',(e) =>
-{
-  const datos = Object.fromEntries(new FormData(form).entries())
-  let imcr = '';
-  let imc = datos.peso / (datos.estatura ** 2);
-  let nombre = datos.nombre;
-  let edad = datos.edad;
-  console.log(datos);
-  if (imc < 19.8) {
-    imcr = 'bajo peso';
-    divInferior.innerHTML = `
+const texto = (imcr,imc,nombre,edad) => {
+  divInferior.innerHTML = `
     <div class="nose">
       <div class="text">
         <p class="peso1">${imcr}</p>
@@ -22,42 +12,26 @@ document.querySelector('#calcular').addEventListener('click',(e) =>
       <img src="./src/storage/baix-pes2.jpg" class="imgs"></img>
       <button type="submit" id="nuevo" class="btn">Nuevo</button>
     </div>`;
+}
+document.querySelector('#calcular').addEventListener('click',(e) =>{
+  const datos = Object.fromEntries(new FormData(form).entries())
+  let imcr = '';
+  let imc = datos.peso / (datos.estatura ** 2);
+  let nombre = datos.nombre;
+  let edad = datos.edad;
+  console.log(datos);
+  if (imc < 19.8) {
+    imcr = 'bajo peso';
+    texto(imcr,imc,nombre,edad);
   } else if (imc >= 19.9 && imc <= 26) {
     imcr = 'peso normal';
-    divInferior.innerHTML = `
-    <div class="nose">
-      <div class="text">
-        <p class="peso1">${imcr}</p>
-        <p class="imc">${imc}</p>
-        <p class="imc">${nombre} ${edad}</p>
-      </div>
-      <img src="./src/storage/pesonormal.jpg" class="imgs"></img>
-      <button type="submit" id="nuevo" class="btn">Nuevo</button>
-    </div>`;
+    texto(imcr,imc,nombre,edad);
   } else if (imc >= 26.1 && imc <= 29) {
     imcr = 'sobrepeso';
-    divInferior.innerHTML = `
-    <div class="nose">
-      <div class="text">
-        <p class="peso1">${imcr}</p>
-        <p class="imc">${imc}</p>
-        <p class="imc">${nombre} ${edad}</p>
-      </div>
-      <img src="./src/storage/images.jpeg" class="imgs"></img>
-      <button type="submit" id="nuevo" class="btn">Nuevo</button>
-    </div>`;
+    texto(imcr,imc,nombre,edad);
   } else if (imc >= 29.1) {
     imcr = 'obesidad';
-    divInferior.innerHTML = `
-    <div class="nose">
-      <div class="text">
-        <p class="peso1">${imcr}</p>
-        <p class="imc">${imc}</p>
-        <p class="imc">${nombre} ${edad}</p>
-      </div>
-      <img src="./src/storage/obesidad.jpeg" class="imgs"></img>
-      <button type="submit" id="nuevo" class="btn">Nuevo</button>
-    </div>`;
+    texto(imcr,imc,nombre,edad);
   }
   console.log(imcr);
   if ([...form.elements]){
@@ -67,10 +41,8 @@ document.querySelector('#calcular').addEventListener('click',(e) =>
       }
     })
   }
-    
   e.preventDefault();
 })
-
 divInferior.addEventListener('click',(e)=>{
   if(e.target && e.target.id === 'nuevo'){
     divInferior.innerHTML = ``;
